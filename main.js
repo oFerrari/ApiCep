8b4b2587d9b55d381c0cf795c9e0231102197516
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-11-18 22:38:02 -0400

    🚧 Subindo Progresso Tratando Erros

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
+import '@picocss/pico'
+import './style.css'
+
 const formConsultarCep = document.querySelector('#consultarCep');
 const inputCep = formConsultarCep.cep
 const divDados = document.querySelector('#dados');
+const btnConsultarCep = document.querySelector('#btnConsultarCep')
+
+/* const loader = '<a href="#" aria-busy="true"> Consultando CEP...</a>' */
 
 formConsultarCep.addEventListener('submit', function(event){
     event.preventDefault(); //anula comportamento padrão de envio do form
-    alert("enviouuu...");
+    btnConsultarCep.setAttribute('aria-busy', 'true');
     consultarCep(inputCep.value);   //79950970
+    ativaLoader(true)
 })
+
 
 async function consultarCep(cep) {
     let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     let dadosCep = await response.json();
+
+    if(dadosCep.erro){
+        divDados.innerHTML = `<div class="erro">CEP não encontrado!</div>`
+    }else{
+        divDados.innerHTML = `
+        <p>Endereço: ${dadosCep.logradouro}</p>
+        <p>Localidade ${dadosCep.localidade}</p>
+        `
+
+    }
+    ativaLoader(false)
+}
+
+
+function ativaLoader(ativo){
+    if(ativo){
+        btnConsultarCep.setAttribute('aria-busy', 'true');
+        btnConsultarCep.textContent = 'Consultando CEP...'
+    }else{
+        btnConsultarCep.removeAttribute('aria-busy');
+        btnConsultarCep.textContent = 'Consultar'
+    }
+
+   /*  ativaLoader(false) */
     
-    console.log(response)
-    console.log(dadosCep)
-
-    divDados.innerHTML = `
-    <p>Endereço: ${dadosCep.logradouro}</p>
-    <p>Localidade ${dadosCep.localidade}</p>
-    `
-
 }
 
